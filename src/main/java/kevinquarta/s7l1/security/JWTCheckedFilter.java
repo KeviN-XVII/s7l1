949fff253db71d189ee6e19e1530ec9e7350d0c5
KeviN-XVII <kevinquartar@gmail.com>
2026-02-16 17:56:23 +0100

    fine

--- src/main/java/kevinquarta/s7l1/security/JWTCheckedFilter.java
+++ src/main/java/kevinquarta/s7l1/security/JWTCheckedFilter.java
@@ -5,31 +5,48 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kevinquarta.s7l1.excpetions.UnauthroizedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
 public class JWTCheckedFilter extends OncePerRequestFilter {
+
+    private final JWTTools jwtTools;
+
+    @Autowired
+    public JWTCheckedFilter(JWTTools jwtTools) {
+        this.jwtTools = jwtTools;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 //        QUESTO E' IL METODO ESEGUITO AD OGNI RICHIESTA
         // Sarà questo metodo quindi che dovrà fare il controllo dei token
 
-        // PIANO DI BATTAGLIA
-
         // 1. Verifichiamo se la richiesta contiene l'header Authorization e che in caso sia nel formato "Bearer oi1j3oj21o3j213jo12j3"
         // Se l'header non c'è oppure se è malformato --> lanciamo eccezione
         String authHeader = request.getHeader("Authorization");
-        if(authHeader == null) throw new UnauthroizedException("Inserire il token")
+        if(authHeader == null|| !authHeader.startsWith("Bearer "))
+            throw new UnauthroizedException("Inserire il token nell'Authorization header");
 
         // 2 estraiamo il token dall'header
+        String accessToken = authHeader.replace("Bearer ", "");
 
         // 3 verifichiamo se il token è valido ( controllo firma e data scadenza)
-
+        jwtTools.verifyToken(accessToken);
         // 4. Se tutto è OK --> andiamo avanti, trasmettiamo la richiesta al prossimo (può essere o un altro elemento della catena oppure il controller)
         filterChain.doFilter(request, response);
         // 5. Se c'è qualche problema con il token -> eccezione
+    }
+    // Tramite l'Override del metodo sottostante posso specificare quando il filtro non debba essere chiamato in causa
+    // Ad esempio posso dirgli di non filtrare tutte le richieste dirette al controller "/auth"
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return new AntPathMatcher().match("/auth/**", request.getServletPath());
+       // questo esclude direttamente tutti gli endpoint con /auth/..
     }
 }
--- src/main/java/kevinquarta/s7l1/security/JWTTools.java
+++ src/main/java/kevinquarta/s7l1/security/JWTTools.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import kevinquarta.s7l1.entities.Dipendente;
+import kevinquarta.s7l1.excpetions.UnauthroizedException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -20,11 +21,21 @@ public class JWTTools {
        return Jwts.builder().issuedAt(new Date(System.currentTimeMillis())) // Data di emissione (IaT - Issued At), va messa in millisecondi
                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)) // Data di scadenza (Expiration Date) anche questa va messa in millisecondi
                .subject(String.valueOf(dipendente.getId())) // Subject cioè a chi appartiene il token. Ci inseriamo l'id dell'utente (MAI METTERE DATI SENSIBILI AL SUO INTERNO)
-               .signWith(Keys.hmacShaKeyFor(secret.getBytes())) //firmo il token fornendogli un segreto che il server conosce e che usa per creare e verificare token
+               .signWith(Keys.hmacShaKeyFor(secret.getBytes())) //firmo il token fornendogli un segreto che il server conosce e che usa per creare e verificare token/verifica l'integrità del token,in modo che controlli che non sia stato manipolato
                .compact();
     }
 
-    public void verifyToker(){
+    public void verifyToken(String token){
+        try {
+            Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(token);
+            // Questi metodi ci possono lanciare diverse eccezioni, a seconda della problematica
+            // Ci può lanciare una certa eccezione se il token dovesse essere scaduto,
+            // un'altra se il token è stato manipolato (firma non valida)
+            // un'altra ancora se il token dovesse essere malformato (es. manca una parte)
+        } catch (Exception ex) {
+            throw new UnauthroizedException("Problemi col token!");
+        }
+
     }
 
 }
